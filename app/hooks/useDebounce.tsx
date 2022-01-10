087aa7aeab631768f0229e239b48a4660d789e6a
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-01-10 21:34:36 +0100

    Initial Commit: Porting to Remix

---
+++ app/hooks/useDebounce.tsx
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from "react";
+
+//@ts-ignore
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
